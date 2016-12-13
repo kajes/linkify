c81ef768ad7e5c10af3aafc17b95654a852dd327
kajes <lars@kajes.se>
2016-12-13 14:49:11 +0100

    Added required attribute to relevant fields

--- resources/blocks/authentication.php
+++ resources/blocks/authentication.php
@@ -3,16 +3,17 @@
   <h2>Sign in to Linkify:</h2>
 
   <h4 class="formLabel">Email or username:</h4>
-  <input type="text" class="login username">
+  <input type="text" class="login username" required>
 
   <h4 class="formLabel">Password:</h4>
-  <input type="password" class="login password">
+  <input type="password" class="login password" required>
 
   <input type="submit" class="login submit" value="Sign in">
 
   <?php
     if (isset($loginError)) {
       echo '<h5 class="error">'.$loginError.'</h5>';
+      unset($loginError);
     }
   ?>
 
@@ -23,22 +24,23 @@
   <h2>Register here:</h2>
 
   <h4 class="formLabel">Full name:</h4>
-  <input type="text" class="register fullName">
+  <input type="text" class="register fullName" required>
 
   <h4 class="formLabel">Email:</h4>
-  <input type="email" class="register email">
+  <input type="email" class="register email" required>
 
   <h4 class="formLabel">Password:</h4>
-  <input type="password" class="register password">
+  <input type="password" class="register password" required>
 
   <h4 class="formLabel">Re-enter password</h4>
-  <input type="password" class="register passwordReenter">
+  <input type="password" class="register passwordReenter" required>
 
   <input type="submit" class="register registerSubmit" value="Sign up">
 
   <?php
     if (isset($registerError)) {
       echo '<h5 class="error">'.$registerError.'</h5>';
+      unset($registerError);
     }
   ?>
 
