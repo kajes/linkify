4d03f760d090d30dcbadfa133b46638a244b687c
kajes <lars@kajes.se>
2016-12-13 09:40:42 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+// Get all the functions
+require_once 'resources/lib/functions.php';
+
+// Get the main template
+require_once 'resources/blocks/home.php';
---
+++ resources/blocks/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php require_once 'head.php'; ?>
+</head>
+<body>
+  <main class="mainWrapper">
+    <header class="mainHeader">
+      <?php require_once 'components/header.php'; ?>
+    </header>
+    <section class="mainContent">
+
+    </section>
+    <footer class="mainFooter">
+      <?php require_once 'components/footer.php'; ?>
+    </footer>
+  </main>
+</body>
+</html>
