f2cd04f539522e43c31643db9d05af9b31bda544
Lars Karlsson <lars@kajes.se>
2017-01-16 20:49:49 +0100

    Fixed bug with redirect to non-existing post

--- resources/blocks/home.php
+++ resources/blocks/home.php
@@ -4,7 +4,11 @@
     if (!isset($_GET['postID'])) {
       require_once 'components/basePosts.php';
     } else {
-      require_once 'components/singlePost.php';
+      if (!$dbConnection->query("SELECT * FROM posts WHERE postID = {$_GET['postID']} LIMIT 1")->fetch()) {
+        header('Location: /');
+      } else {
+        require_once 'components/singlePost.php';
+      }
     }
   ?>
 
